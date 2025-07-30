231cbf764631b3b4e8c472b888939083d41658f1
sofiaAguirre20 <aguirre.ayelen.sofia@gmail.com>
2025-07-30 16:49:08 -0300

    se logro darle ancho y posicion a los mensajes

--- src/Components/Message/Message.jsx
+++ src/Components/Message/Message.jsx
@@ -5,11 +5,15 @@ import ICONS from '../../Constants/Icons'
 const Message = ({emisor, hora, id, texto, status,deleteMessageById}) => {
 
   let message_container_class = 'message-container'
+  let message_data
 
   if(emisor === 'YO'){
     message_container_class += ' my-message'
+    message_data = 'my-message-data'
+
   }else{
     message_container_class += ' other-message'
+    message_data = 'other-message-data'
   }
     return(
       // <section className='chat-container'>
@@ -24,7 +28,7 @@ const Message = ({emisor, hora, id, texto, status,deleteMessageById}) => {
        
        <div className={message_container_class}>
        
-         <div className='message'>
+         <div className={message_data}>
           <span className='text'>{texto}</span>
           <span className='time'>{hora}</span>
           
