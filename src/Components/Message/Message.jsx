23146d8be0751a7f0b2e147702191e106939fd48
sofiaAguirre20 <aguirre.ayelen.sofia@gmail.com>
2025-08-01 15:12:47 -0300

    añadi boton para eliminar mensaje

--- src/Components/Message/Message.jsx
+++ src/Components/Message/Message.jsx
@@ -16,25 +16,18 @@ const Message = ({emisor, hora, id, texto, status,deleteMessageById}) => {
     message_data = 'other-message-data'
   }
     return(
-      // <section className='chat-container'>
-      //   <div className='text-time-container'>
-
-      //     <p className='text'>{texto}</p>
-      //     <span className='time'>{hora}</span>
-          
-      //   </div>
-      //   <button onClick={() => {deleteMessageById(id)}} className='button-deleteMessage'><ICONS.DeleteContact className='button-delete'/></button>
-      // </section>
+     
        
-       <div className={message_container_class}>
+      <div className={message_container_class}>
        
-         <div className={message_data}>
+        <div className={message_data}>
           <span className='text'>{texto}</span>
           <span className='time'>{hora}</span>
           
       
-        {/* <button onClick={() => {deleteMessageById(id)}} className='button-deleteMessage'><ICONS.DeleteContact className='button-delete'/></button> */}
-       </div>
+         
+        </div>
+        <button onClick={() => {deleteMessageById(id)}} className='button-deleteMessage'><ICONS.DeleteContact className='button-delete'/></button>
       </div>
     )
 }
