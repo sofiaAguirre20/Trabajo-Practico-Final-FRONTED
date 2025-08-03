bac0efe4473cedde0992595eed1d6169e7255919
sofiaAguirre20 <aguirre.ayelen.sofia@gmail.com>
2025-08-03 13:33:49 -0300

    refactorizacion de mensajes y estilado

--- src/services/contactServices.js
+++ src/services/contactServices.js
@@ -86,94 +86,87 @@ const contacts = [
         connection_status: 'offline',
         messages:[
             {
-                emisor: 'YO',
+                emisor: 'OTRO',
                 hora: '23:10',
                 id: 1,
-                texto: 'Holisss',
-                status: 'visto'
-            },
-            {
-                emisor: 'OTRO',
-                hora: '23:11',
-                id: 2,
                 texto: 'Hola, tanto tiempo!!',
                 status: 'visto'
             },
             {
                 emisor: 'OTRO',
-                hora: '23:12',
+                hora: '23:10',
                 id: 3,
                 texto: 'Como has estado?',
                 status: 'visto'
             },
             {
                 emisor: 'OTRO',
-                hora: '23:12',
+                hora: '23:10',
                 id: 4,
                 texto: 'Siempre me pregunte que ha sido de ti',
                 status: 'visto'
             },
             {
                 emisor: 'YO',
-                hora: '23:12',
+                hora: '23:14',
                 id: 5,
-                texto: 'Si paso mucho!',
+                texto: 'Ey!!. Si la verdad, paso mucho!',
                 status: 'visto'
             },
             {
                 emisor: 'YO',
-                hora: '23:12',
+                hora: '23:15',
                 id: 6,
                 texto: 'Todo tranquilo, ya me recibí y ahora ya estoy trabajando',
                 status: 'visto'
             },
             {
                 emisor: 'YO',
-                hora: '23:12',
+                hora: '23:15',
                 id: 7,
                 texto: 'Vos que me contas?',
                 status: 'visto'
             },
             {
                 emisor: 'OTRO',
-                hora: '23:12',
+                hora: '23:17',
                 id: 8,
                 texto: 'Por ahora estoy sin empleo',
                 status: 'visto'
             },
             {
                 emisor: 'OTRO',
-                hora: '23:12',
+                hora: '23:17',
                 id: 9,
                 texto: 'Estuve buscando pero aun nada',
                 status: 'visto'
             },
             {
-                emisor: 'YO',
-                hora: '23:12',
+                emisor: 'OTRO',
+                hora: '23:17',
                 id: 10,
                 texto: 'Espero tener suerte',
                 status: 'visto'
             },
             {
                 emisor: 'YO',
-                hora: '23:12',
+                hora: '23:20',
                 id: 11,
                 texto: 'Si seguro',
                 status: 'visto'
             },
             {
                 emisor: 'YO',
-                hora: '23:12',
+                hora: '23:20',
                 id: 12,
                 texto: 'Yo confio que si',
                 status: 'visto'
             },
             {
                 emisor: 'YO',
-                hora: '23:12',
+                hora: '23:20',
                 id: 13,
-                texto: 'Asi que confia tambien',
+                texto: 'Arriba ese ánimo!!',
                 status: 'visto'
             }
         ]
