ffd4c79a32f38dec8db5a6a0d64d10664c644644
sofiaAguirre20 <aguirre.ayelen.sofia@gmail.com>
2025-08-03 00:19:12 -0300

    agrego la función onChange en el componente FilteredContact

--- src/Components/FilteredContact/FilteredContact.jsx
+++ src/Components/FilteredContact/FilteredContact.jsx
@@ -1,12 +1,22 @@
+import React, {useState} from 'react'
 import ICONS from '../../Constants/Icons'
 import './FilteredContact.css'
 import { getContactList } from '../../services/contactServices'
 
 
 
-const FilteredContact = (contact_selected) =>{
+const FilteredContact = (contact_selected) => {
 
-   const contacts = getContactList()
+    const [inputValue, setInputValue] = useState('');
+
+    const handleChange = (event) => {
+
+        console.log('VALUEEEEEEEEEEE', event.target.value);
+        setInputValue(event.target.value);
+    };
+
+
+    const contacts = getContactList()
 
     const filtered_contacts = contacts.filter((contact) => {
         const filter_text = ""
@@ -15,12 +25,16 @@ const FilteredContact = (contact_selected) =>{
         return contact_name.toLocaleLowerCase().includes(filter_text.toLowerCase().trim())
 
     })
-    return(
+
+
+
+    return (
 
         <form className='filter-contact'>
             <label htmlFor='filter' className='label-filter'></label>
-            <div className='search-icon-container'><ICONS.searchContact className='search-icon'/></div>
-            <input  type='text' placeholder='Buscar un chat o iniciar uno nuevo' id='filter' name='filtered-contact' autoComplete='off' required/>
+            <div className='search-icon-container'><ICONS.searchContact className='search-icon' /></div>
+            <input type='text' value={inputValue}
+                onChange={handleChange} placeholder='Buscar un chat o iniciar uno nuevo' id='filter' name='filtered-contact' autoComplete='off' required />
         </form>
     )
 
